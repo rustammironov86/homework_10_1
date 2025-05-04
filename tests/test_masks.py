e71726a27067c9236a3c0b90eb3a82a0669234cd
rustam_mironov <tatapuh9801@gmail.com>
2025-05-05 04:59:33 +0500

    add test moduls index.html change moduls

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 """Импортируем необходимые функции и pytest"""
 
