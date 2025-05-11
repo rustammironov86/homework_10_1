062e4ea0ae9cab8e901663923b7f2de0b5e0ed24
rustam_mironov <tatapuh9801@gmail.com>
2025-05-11 17:16:10 +0500

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from src import processing
+
+
+if __name__ == "__main__":
+    data_operation = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+    date_list = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+    filter_data_operation = processing.filter_by_state(data_operation, state="EXECUTED")
+    filter_sort_by_date = processing.sort_by_date(date_list, reverse=True)
+
+    print(filter_data_operation)
+    print(filter_sort_by_date)
