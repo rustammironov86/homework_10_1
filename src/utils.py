61111de317f2f4622120548a9cec6ef242409f1d
rustam_mironov <tatapuh9801@gmail.com>
2025-06-02 22:19:45 +0500

    add homework_12_2 done add logs masks and utils

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,34 @@
 import json
 import os
+import logging
+from config import BASE_DIR
 
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(BASE_DIR + "/logs/utils.log", encoding="utf-8", mode="w")
+file_formatter = logging.Formatter("%(asctime)s (%(filename)s).%(funcName)s %(levelname)s %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def correct_json_file(json_file):
     """Функция проверяет наличие файла с транзакциями и возвращает их"""
     try:
+        logger.debug("Выполняется обращение к файлу с транзакциями")
         with open(json_file, encoding="utf-8") as file:
             file_json = json.load(file)
             return file_json
     except FileNotFoundError:
+        logger.error("Файл не найден")
         print("Файл не найден")
         return []
     except json.JSONDecodeError:
+        logger.error("Ошибка декодирования")
         print("Ошибка декодирования JSON")
         return []
     except Exception as e:
+        logger.error(f"Произошла ошибка: {e}")
         print(f"Произошла ошибка: {e}")
 
 
